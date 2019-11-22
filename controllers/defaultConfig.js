48e37de0bd340abfa9cfff31d8ae1c0f5bb530af
Danking <228731007@qq.com>
2019-11-22 11:23:44 +0800

    init

---
+++ controllers/defaultConfig.js
@@ -0,0 +1,13 @@
+//defaultConfig.js
+const config = {
+    // 数据库配置
+    database: {
+        DATABASE: 'xxx', //数据库名称
+        USERNAME: 'xxx', //mysql用户名
+        PASSWORD: 'xxx', //mysql密码
+        PORT: '3306', //mysql端口号
+        HOST: 'xx.xxx.xx.xx' //服务器ip
+    }
+}
+
+module.exports = config
